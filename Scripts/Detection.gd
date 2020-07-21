8157cb4f4ac51f1234cf7d5c0e9976dc34050c99
Tabitha Chang <changt6@bxscience.edu>
2020-07-21 16:03:13 -0400

    Game is Pausable

--- Scripts/Detection.gd
+++ Scripts/Detection.gd
@@ -3,6 +3,7 @@ extends RayCast2D
 
 
 var processing = false
+var pausescreen = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +19,18 @@ func _input(event):
 				var node = scene.instance()
 				self.get_parent().get_node("Display/CanvasLayer/Overlay").add_child(node)
 				Global.freeze = true
+	if(event is InputEventKey and event.pressed):
+		if(Input.is_key_pressed(KEY_ESCAPE)):
+			if not Global.freeze and not pausescreen:
+				Global.freeze = true
+				pausescreen=true
+				var scene = preload("res://Scenes/Pause.tscn")
+				var node = scene.instance()
+				self.get_parent().get_node("Display/CanvasLayer/Overlay").add_child(node)
+			elif Global.freeze and pausescreen:
+				Global.freeze=false
+				pausescreen=false
+				self.get_parent().get_node("Display/CanvasLayer/Overlay/Pause").queue_free()
 
 
 
@@ -34,7 +47,7 @@ func _process(delta):
 			t.start()
 			yield(t, "timeout")
 			t.queue_free()
-		elif(self.get_collider().get_parent().name=="Monsters"):
+		elif(self.get_collider().get_parent().get_parent().name=="Monsters"):
 			Global.freeze=true
 			self.get_collider().queue_free()
 			var scene = preload("res://Scenes/Fight.tscn")
