3c7dd23d77c2fb15d98e05bd51512720a867a174
Tabitha Chang <changt6@bxscience.edu>
2020-07-22 01:27:24 -0400

    player accounts for 4 person party

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,18 +9,38 @@ var tedspd = 5
 var tedmaxmp = 8
 var tedmp = 8
 var tedstr = 3
-var teddef = 3
-var tedweap = 5
+var teddef = 2
+var tedweap = 3
 
 var maddymaxhp = 8
 var maddyhp = 8
 var maddyxp = 0
 var maddyspd = 5
-var maddymaxmp = 10
-var maddymp = 10
+var maddymaxmp = 15
+var maddymp = 15
 var maddystr = 1
 var maddydef = 1
 var maddyweap = 3
+
+var sammaxhp = 9
+var samhp = 9
+var samxp = 0
+var samspd = 5
+var sammaxmp = 10
+var sammp = 10
+var samstr = 2
+var samdef = 1
+var samweap = 3
+
+var rosemaxhp = 9
+var rosehp = 9
+var rosexp = 0
+var rosespd = 5
+var rosemaxmp = 8
+var rosemp = 8
+var rosestr = 2
+var rosedef = 2
+var roseweap = 3
 
 var incapacitated = false
 var invulnerable = false
@@ -74,6 +94,8 @@ func _physics_process(delta):
 		self.position=lastheal
 		tedhp = tedmaxhp
 		maddyhp=maddymaxhp
+		samhp=sammaxhp
+		rosehp=rosemaxhp
 		incapacitated=false
 		self.get_node("Display/CanvasLayer/Labels").set_text("Ted HP:"+(str(tedhp)+"\n"+"Maddy HP:"+(str(maddyhp))+"\n"+"GP:"+str(gp)))
 	if Global.freeze:
@@ -98,14 +120,27 @@ func dealDamage(damage, target):
 		tedhp-=damage
 		if tedhp<=0:
 			tedhp=0
-			changeLeader(1)
 	elif(target=="Maddy"):
 		maddyhp-=damage
 		if maddyhp<=0:
 			maddyhp=0
-			changeLeader(0)
+	elif(target=="Rose"):
+		rosehp-=damage
+		if rosehp<=0:
+			rosehp=0
+	elif(target=="Sam"):
+		samhp-=damage
+		if samhp<=0:
+			samhp=0
 	if(maddyhp == 0 and tedhp==0):
-		incapacitated=true
+		if not Global.samexist and not Global.roseexist:
+			incapacitated=true
+		elif(samhp==0 and not Global.roseexist):
+			incapacitated=true
+		elif(rosehp==0 and not Global.samexist):
+			incapacitated=true
+		elif(rosehp==0 and samhp==0):
+			incapacitated=true
 	self.get_node("Display/CanvasLayer/Labels").set_text("Ted HP:"+(str(tedhp)+"\n"+"Maddy HP:"+(str(maddyhp))+"\n"+"GP:"+str(gp)))
 
 func useMP(amount, target) -> bool:
@@ -123,6 +158,20 @@ func useMP(amount, target) -> bool:
 		else:
 			maddymp+=amount
 			return false
+	elif(target=="Sam"):
+		sammp-=amount
+		if(sammp>=0):
+			return true
+		else:
+			sammp+=amount
+			return false
+	elif(target=="Rose"):
+		rosemp-=amount
+		if(rosemp>=0):
+			return true
+		else:
+			rosemp+=amount
+			return false
 	return false
 		
 func restoreMP(amount, target):
@@ -134,6 +183,14 @@ func restoreMP(amount, target):
 		maddymp+=amount
 		if maddymp>maddymaxmp:
 			maddymp=maddymaxmp
+	elif(target=="Sam"):
+		sammp+=amount
+		if sammp>sammaxmp:
+			sammp=sammaxmp
+	elif(target=="Rose"):
+		rosemp+=amount
+		if rosemp>rosemaxmp:
+			rosemp=rosemaxmp
 
 func healchar(amount, target):
 	if(target=="Ted"):
@@ -144,11 +201,21 @@ func healchar(amount, target):
 		maddyhp+=amount
 		if maddyhp>maddymaxhp:
 			maddyhp=maddymaxhp
+	elif(target=="Sam"):
+		samhp+=amount
+		if samhp>sammaxhp:
+			samhp=sammaxhp
+	elif(target=="Rose"):
+		rosehp+=amount
+		if rosehp>rosemaxhp:
+			rosehp=rosemaxhp
 	self.get_node("Display/CanvasLayer/Labels").set_text("Ted HP:"+(str(tedhp)+"\n"+"Maddy HP:"+(str(maddyhp))+"\n"+"GP:"+str(gp)))
 
 func heal(amount):
 	healchar(amount, "Ted")
 	healchar(amount, "Maddy")
+	healchar(amount, "Sam")
+	healchar(amount, "Rose")
 	self.get_node("Display/CanvasLayer/Labels").set_text("Ted HP:"+(str(tedhp)+"\n"+"Maddy HP:"+(str(maddyhp))+"\n"+"GP:"+str(gp)))
 
 func resetHealth():
@@ -157,6 +224,10 @@ func resetHealth():
 	tedmp=tedmaxmp
 	maddyhp=maddymaxhp
 	maddymp=maddymaxmp
+	samhp=sammaxhp
+	sammp=sammaxmp
+	rosehp=rosemaxhp
+	rosemp=rosemaxmp
 	self.get_node("Display/CanvasLayer/Labels").set_text("Ted HP:"+(str(tedhp)+"\n"+"Maddy HP:"+(str(maddyhp))+"\n"+"GP:"+str(gp)))
 
 func doTransaction(amount, type) ->bool:
@@ -178,6 +249,10 @@ func doTransaction(amount, type) ->bool:
 func addXP(amount):
 	tedxp+=amount
 	maddyxp+=amount
+	if(Global.samexist):
+		samxp+=amount
+	if(Global.roseexist):
+		rosexp+=amount
 
 
 func overworld_damage(amount):
@@ -185,6 +260,10 @@ func overworld_damage(amount):
 		invulnerable = true
 		dealDamage(amount, "Ted")
 		dealDamage(amount, "Maddy")
+		if(Global.roseexist):
+			dealDamage(amount, "Rose")
+		if(Global.samexist):
+			dealDamage(amount, "Sam")
 		self.modulate.a = 0.5
 		var t = Timer.new() #time spent being invulnerable
 		t.set_wait_time(0.3)
