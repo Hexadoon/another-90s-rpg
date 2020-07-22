d2e01b36a923561e2762c0a08f52503716c0119b
Tabitha Chang <changt6@bxscience.edu>
2020-07-22 01:26:56 -0400

    pause reflects 4 person party

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -22,6 +22,26 @@ func _ready():
 	self.get_node("Stats/Ted").add_text("Ted XP: " + str(player.tedxp)+"\n")
 	self.get_node("Stats/Ted").add_text("Ted STR: " + str(player.tedstr)+"\n")
 	self.get_node("Stats/Ted").add_text("Ted DEF: " + str(player.teddef))
+	
+	self.get_node("Stats/Sam").set_text("Sam HP: " + str(player.samhp)+"/"+str(player.sammaxhp)+"\n")
+	self.get_node("Stats/Sam").add_text("Sam MP: " + str(player.sammp)+"/"+str(player.sammaxmp)+"\n")
+	self.get_node("Stats/Sam").add_text("Sam XP: " + str(player.samxp)+"\n")
+	self.get_node("Stats/Sam").add_text("Sam STR: " + str(player.samstr)+"\n")
+	self.get_node("Stats/Sam").add_text("Sam DEF: " + str(player.samdef))
+	
+	self.get_node("Stats/Rose").set_text("Rose HP: " + str(player.rosehp)+"/"+str(player.rosemaxhp)+"\n")
+	self.get_node("Stats/Rose").add_text("Rose MP: " + str(player.rosemp)+"/"+str(player.rosemaxmp)+"\n")
+	self.get_node("Stats/Rose").add_text("Rose XP: " + str(player.rosexp)+"\n")
+	self.get_node("Stats/Rose").add_text("Rose STR: " + str(player.rosestr)+"\n")
+	self.get_node("Stats/Rose").add_text("Rose DEF: " + str(player.rosedef))
+	
+	
+	
+	if(Global.samexist):
+		self.get_node("Stats/Sam").visible=true
+	if(Global.roseexist):
+		self.get_node("Stats/Rose").visible=true
+	
 	pass # Replace with function body.
 
 
