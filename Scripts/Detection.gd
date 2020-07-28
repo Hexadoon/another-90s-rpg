a85495a7ac95fa60f6e2853b97bc89226b57a9d7
Tabitha Chang <changt6@bxscience.edu>
2020-07-28 00:32:34 -0400

    sam & rose have text, can change locations

--- Scripts/Detection.gd
+++ Scripts/Detection.gd
@@ -20,15 +20,63 @@ func _input(event):
 				self.get_parent().get_node("Display/CanvasLayer/Overlay").add_child(node)
 				Global.freeze = true
 			if(self.get_collider().name=="Sam"):
+				Global.freeze=true
 				Global.samexist=true
+				self.get_parent().get_node("Display/CanvasLayer/TextBox").visible=true
+				self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Madeline: Sam! There you are!")
+				var t = Timer.new() #time spent being invulnerable
+				t.set_wait_time(4)
+				t.set_one_shot(true)
+				self.add_child(t)
+				t.start()
+				yield(t, "timeout")
+				self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Sam: Have you seen my Magnemite card? I think I dropped it when I was looking for my De-Spell card...")
+				t.start()
+				yield(t, "timeout")
+				self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Madeline: Sam, you can't go running off into the woods alone to look for a card! You could get yourself killed!")
+				t.start()
+				yield(t, "timeout")
+				self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Sam Joined the Party!")
+				t.start()
+				yield(t, "timeout")
+				self.get_parent().get_node("Display/CanvasLayer/TextBox").visible=false
 				self.get_collider().visible=false
 				self.get_collider().position = Vector2(-1000,-1000)
+				t.queue_free()
 				self.get_collider().queue_free()
+				Global.freeze=false
 			if(self.get_collider().name=="Rose"):
-				Global.roseexist=true
-				self.get_collider().visible=false
-				self.get_collider().position = Vector2(-1000,-1000)
-				self.get_collider().queue_free()
+				if(Global.samexist):
+					Global.freeze=true
+					Global.roseexist=true
+					self.get_parent().get_node("Display/CanvasLayer/TextBox").visible=true
+					self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Rose: Heh, did you think I was a mushroom before?")
+					var t = Timer.new() #time spent being invulnerable
+					t.set_wait_time(3)
+					t.set_one_shot(true)
+					self.add_child(t)
+					t.start()
+					yield(t, "timeout")
+					self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Madeline: You were a very convincing mushroom")
+					t.start()
+					yield(t, "timeout")
+					self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Rose: Think I could trick real monsters with my ninja skills?")
+					t.start()
+					yield(t, "timeout")
+					self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Madeline: If you're going to try, be careful, okay?")
+					t.start()
+					yield(t, "timeout")
+					self.get_parent().get_node("Display/CanvasLayer/TextBox/TextBoxText").set_text("Rose Joined the Party!")
+					t.start()
+					yield(t, "timeout")
+					self.get_parent().get_node("Display/CanvasLayer/TextBox").visible=false
+					self.get_collider().visible=false
+					self.get_collider().position = Vector2(-1000,-1000)
+					t.queue_free()
+					self.get_collider().queue_free()
+					Global.freeze=false
+				else:
+					self.get_collider().get_node("Sprite").play("popup")
 
 			
 	if(event is InputEventKey and event.pressed):
@@ -65,6 +113,30 @@ func _process(delta):
 			var scene = preload("res://Scenes/Fight.tscn")
 			var node = scene.instance()
 			self.get_parent().get_node("Display/CanvasLayer/Overlay").add_child(node)
+			Global.setting="Fight"
 			Global.freeze = true
+		elif(self.get_collider().name=="Forestpath"):
+			Global.setting="Forest"
+			self.get_parent().get_parent().get_node("Background").get_child(0).queue_free()
+			var scene = preload("res://Scenes/ForestBackground.tscn")
+			var node = scene.instance()
+			self.get_parent().get_parent().get_node("Background").add_child(node)
+			self.get_parent().get_parent().get_node("Foreground").get_child(0).queue_free()
+			scene = preload("res://Scenes/ForestForeground.tscn")
+			node = scene.instance()
+			self.get_parent().get_parent().get_node("Foreground").add_child(node)
+			self.get_parent().position=Vector2(747,1900)
+		elif(self.get_collider().name=="Townpath"):
+			print(str(self.get_parent().position))
+			Global.setting="Town"
+			self.get_parent().get_parent().get_node("Background").get_child(0).queue_free()
+			var scene = preload("res://Scenes/TownBackground.tscn")
+			var node = scene.instance()
+			self.get_parent().get_parent().get_node("Background").add_child(node)
+			self.get_parent().get_parent().get_node("Foreground").get_child(0).queue_free()
+			scene = preload("res://Scenes/TownForeground.tscn")
+			node = scene.instance()
+			self.get_parent().get_parent().get_node("Foreground").add_child(node)
+			self.get_parent().position=Vector2(601,133)
 
 		processing = false
